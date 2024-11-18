25cec2bb8ad839b3c3114362979dde69fcc6f4b2
Galichenko <e.galichenko@3owls.ru>
2024-11-18 23:55:23 +0300

    deploy

---
+++ _nuxt/CMhI__5i.js
@@ -0,0 +1,1 @@
+import{_ as s,c as t,a8 as a,o}from"./-uZEn89d.js";const c={},n={class:"main-page"};function r(e,_){return o(),t("div",n,[a(e.$slots,"default")])}const d=s(c,[["render",r]]);export{d as default};
---
+++ _nuxt/CmrOEUzx.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./27ybnZAO.js";import"./-uZEn89d.js";export{m as default};
